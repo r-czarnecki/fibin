f31c8cbcf173d98644ca1b1ae994f80649a38024
Rafal <r.czarnecki6@student.uw.edu.pl>
2019-12-02 19:51:36 +0100

    Added Lit and Var

--- fibin.h
+++ fibin.h
@@ -1,6 +1,10 @@
 #ifndef FIBIN_H
 #define FIBIN_H
 
+#include <type_traits>
+#include <cassert>
+using namespace std;
+
 template <typename ValueType>
 class Fibin {
 public:
@@ -34,8 +38,9 @@ protected:
 template<int N>
 class Fib {
 public:
-	constexpr static unsigned generate() {
-		unsigned fs = 0, sn = 1, t = 0;
+	typedef bool lit;
+	constexpr static unsigned long long generate() {
+		unsigned long long fs = 0, sn = 1, t = 0;
 		if (N < 2)
 			return N;
 		for (int i=1; i<N; i++) {
@@ -49,6 +54,7 @@ public:
 
 class True {
 public:
+	typedef bool lit;
 	constexpr static bool generate() {
 		return true;
 	}
@@ -56,10 +62,37 @@ public:
 
 class False {
 public:
+	typedef bool lit;
 	constexpr static bool generate() {
 		return false;
 	}
 };
 
+template <typename T, typename T::lit = 0>
+class Lit : public Fibin<unsigned long long> {
+	constexpr Lit() 
+	: Fibin() {
+		hasValue = true; 
+		value = T::generate();
+	}
+};
+
+constexpr unsigned long long Var(const char* str) {
+	unsigned long long result = 1;
+	for(int i = 0; str[i] != '\0'; i++) { 
+		char c = str[i];
+		if(str[i] >= 'A' && str[i] <= 'Z')
+			c += 32;
+		
+		assert(((void)"Illegal character in Var(const char*)",
+			   (str[i] >= 'a' && str[i] <= 'z') || (str[i] >= '0' && str[i] <= '9')));
+		assert(((void)"Argument in Var(const char*) is too long", i < 6));
+
+		result *= 100;
+		result += c;
+	}
+
+	return result;
+}
 
 #endif /* FIBIN_H */
