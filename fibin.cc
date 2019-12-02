e352ac9412aaf368920c81a4737886ef54027132
Kejmer <kajetanhus@gmail.com>
2019-12-02 12:51:20 +0100

    definicje w cc

--- fibin.cc
+++ fibin.cc
@@ -4,6 +4,22 @@
 
 using namespace std; 
 
+template<int N>
+class Fib {
+public:
+    constexpr static unsigned generate() {
+        unsigned fs = 0, sn = 1, t;
+        if (N < 2)
+            return N;
+        for (int i=1; i<N; i++) {
+            t = sn + fs;
+            fs = sn;
+            sn = t;
+        }
+        return t;
+    }
+};
+
 int main() {
     const unsigned temp = Fib<3>::generate();
     cout << temp << endl;
--- fibin.h
+++ fibin.h
@@ -1,21 +1,7 @@
 #ifndef FIBIN_H
 #define FIBIN_H
 
-template<int N>
-class Fib {
-public:
-	constexpr static unsigned generate() {
-		unsigned fs = 0, sn = 1, t;
-		if (N < 2)
-			return N;
-		for (int i=1; i<N; i++) {
-			t = sn + fs;
-			fs = sn;
-			sn = t;
-		}
-		return t;
-	}
-};
+template<int N> class Fib;
 
 class True {
 public:
