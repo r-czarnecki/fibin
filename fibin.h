c9935f62e7d8f2e1d14b6d46d82d14b442a6c867
Rafal <r.czarnecki6@student.uw.edu.pl>
2019-12-02 17:35:18 +0100

    Added Fibin class

--- fibin.h
+++ fibin.h
@@ -1,11 +1,37 @@
 #ifndef FIBIN_H
 #define FIBIN_H
 
+template <typename ValueType>
+class Fibin {
+public:
+	template<typename Expression>
+	constexpr static ValueType eval() {
+		return Expression().invoke().getValue();
+	}
+
+protected:
+	constexpr Fibin()
+	: value(0)
+	, invoke(nullptr) {
+		invoke = [] (Fibin& ths, Fibin& arg) -> Fibin& {
+			(void)arg;
+			return ths;
+		};
+	}
+
+	constexpr ValueType getValue() const {
+		return value;
+	}
+
+	ValueType value;
+	Fibin& (*invoke)(Fibin&, Fibin&);
+};
+
 template<int N>
 class Fib {
 public:
 	constexpr static unsigned generate() {
-		unsigned fs = 0, sn = 1, t;
+		unsigned fs = 0, sn = 1, t = 0;
 		if (N < 2)
 			return N;
 		for (int i=1; i<N; i++) {
