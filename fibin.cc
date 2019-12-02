67ee53cdb96d301c80f9795653b60b6610228f04
Kejmer <kajetanhus@gmail.com>
2019-12-02 10:16:58 +0100

    Test for True

--- fibin.cc
+++ fibin.cc
@@ -7,4 +7,7 @@ using namespace std;
 int main() {
     const unsigned temp = Fib<3>::generate();
     cout << temp << endl;
+    
+    const bool b = True::generate();
+    cout << (b ? "Prawda" : "Falsh") << endl;
 }
--- fibin.h
+++ fibin.h
@@ -22,17 +22,14 @@ public:
 	constexpr static bool generate() {
 		return true;
 	}
-}
+};
 
 class False {
 public:
 	constexpr static bool generate() {
 		return false;
 	}
-}
-
-
-
+};
 
 
 #endif /* FIBIN_H */
