9425b60da1275c133a93bb9941e7a9b8d3d366b2
Kejmer <kajetanhus@gmail.com>
2019-12-04 17:34:53 +0100

    struct ma domyslnie public

--- fibin.h
+++ fibin.h
@@ -6,14 +6,14 @@
 using namespace std;
 
 template<typename FIBIN, typename LST, typename ARG>
-class Evaluate {};
+struct Evaluate {};
 
-class ARGNULL {};
+struct ARGNULL {};
 
 //LICZENIE LICZB FIBONACIEGO
 
 template<int N> //Może lepiej zamiast int użyć ValueType z  fibina? Wrzucić do środka i takie tam
-class Fib    {
+struct Fib    {
 public:
 	typedef bool lit;
 	static const int result = Fib<N-1>::result + Fib<N-2>::result;
@@ -21,7 +21,7 @@ public:
 };
 
 template<>
-class Fib<1> {
+struct Fib<1> {
 public:
 	typedef bool lit;
 	static const int result = 1;
@@ -29,7 +29,7 @@ public:
 };
 
 template<>
-class Fib<0> {
+struct Fib<0> {
 public:
 	typedef bool lit;
 	static const int result = 0;
@@ -38,14 +38,14 @@ public:
 
 //WARTOŚCI LOGICZNE
 
-class True {
+struct True {
 public:
 	typedef bool lit;
 	static const bool result = true;
 	static const bool isBoolean = true;
 };
 
-class False {
+struct False {
 public:
 	typedef bool lit;
 	static const bool result = false;
@@ -54,65 +54,65 @@ public:
 
 //LISTA - JESZCZE NIE TESTOWANA
 
-class LNULL {
+struct LNULL {
 	typedef LNULL Head;
 	typedef LNULL Var;
 	typedef LNULL Tail;
 };
 
 template<typename H, typename V, typename T = LNULL>
-class List {
+struct List {
 	typedef H Head;
 	typedef V Var;
 	typedef T Tail;
 };
 
 template<typename H, typename V, typename LST>
-class PushFront {
+struct PushFront {
 	typedef List<H, V, LST> result;
 };
 
 template <typename A, typename B>
-class Eq {
+struct Eq {
 	static const bool result = false;
 };
 
 template<typename A>
-class Eq<A, A> {
+struct Eq<A, A> {
 	static const bool result = true;
 };
 
 template<typename V, typename LST, bool FOUND = false>
-class Find {
+struct Find {
 	typedef Find<V, LST, Eq<V, typename LST::Var>::result> result;
 };
 
 template<typename V, typename LST>
-class Find<V, LST, false> {
+struct Find<V, LST, false> {
 	typedef Find<V, typename LST::Tail, Eq<V, typename LST::Tail::Var>::result> result;
 };
 
 template<typename V, typename LST>
-class Find<V, LST, true> {
+struct Find<V, LST, true> {
 	typedef typename LST::Head result;
 };
 
 template<typename V, bool FOUND>
-class Find<V, LNULL, FOUND> {
+struct Find<V, LNULL, FOUND> {
 	typedef LNULL result;
 };
 
 //LIT
 
 template <typename T, typename T::lit = 0>
-class Lit {
+struct Lit {
 public:
 	static const unsigned long long value = T::result;
 	static const bool isBoolean = T::isBoolean;
 };
 
 template<typename LST, typename ARG, typename T>
-class Evaluate<Lit<T>, LST, ARG> {
+struct Evaluate<Lit<T>, LST, ARG> {
 public:
 	typedef Lit<T> result;
 };
@@ -120,25 +120,25 @@ public:
 //SUM
 
 template<typename FIRST, typename SECOND, typename... ARGS>
-class Sum {
+struct Sum {
 };
 
 template<typename LST, typename ARG, typename FIRST, typename... ARGS>
-class Add {
+struct Add {
 public:
 	static const unsigned long long int value = Add<LST, ARG, FIRST>::value + Add<LST, ARG, ARGS...>::value;
 	static const bool isBoolean = false;
 };
 
 template<typename LST, typename ARG, typename FIRST>
-class Add<LST, ARG, FIRST> {
+struct Add<LST, ARG, FIRST> {
 public:
 	static_assert(!FIRST::isBoolean, "");
 	static const unsigned long long int value = Evaluate<FIRST, LST, ARG>::result::value;
 };
 
 template<typename LST, typename ARG, typename FIRST, typename SECOND, typename... ARGS>
-class Evaluate<Sum<FIRST, SECOND, ARGS...>, LST, ARG> {
+struct Evaluate<Sum<FIRST, SECOND, ARGS...>, LST, ARG> {
 public:
 	typedef Add<LST, ARG, FIRST, SECOND, ARGS...> result;
 };
@@ -146,13 +146,13 @@ public:
 //Inc1
 
 template<typename ARG>
-class Inc1 {
+struct Inc1 {
 public:
 	static const unsigned long long int value = Evaluate<Sum<ARG, Lit<Fib<1>>>, LNULL, ARGNULL>::result::value;
 };
 
 template<typename LST, typename ARG, typename INCARG>
-class Evaluate<Inc1<INCARG>, LST, ARG> {
+struct Evaluate<Inc1<INCARG>, LST, ARG> {
 public:
 	typedef Inc1<INCARG> result;
 };
@@ -160,13 +160,13 @@ public:
 //Inc10
 
 template<typename ARG>
-class Inc10 {
+struct Inc10 {
 public:
 	static const unsigned long long int value = Evaluate<Sum<ARG, Lit<Fib<10>>>, LNULL, ARGNULL>::result::value;
 };
 
 template<typename LST, typename ARG, typename INCARG>
-class Evaluate<Inc10<INCARG>, LST, ARG> {
+struct Evaluate<Inc10<INCARG>, LST, ARG> {
 public:
 	typedef Inc10<INCARG> result;
 };
@@ -194,7 +194,7 @@ constexpr unsigned long long Var(const char* str) {
 //FIBIN
 
 template <typename ValueType>
-class Fibin {
+struct Fibin {
 public:
 	template<typename Expression>
 	constexpr static ValueType eval() {
